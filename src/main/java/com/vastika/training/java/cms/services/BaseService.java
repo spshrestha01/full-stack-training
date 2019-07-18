51243d7a4787ba7222a6b8be6ae01e97c34e9d57
sauravshrestha <spshrestha01@gmail.com>
2019-07-18 00:03:27 -0500

    Removed redundant 'public'

--- src/main/java/com/vastika/training/java/cms/services/BaseService.java
+++ src/main/java/com/vastika/training/java/cms/services/BaseService.java
@@ -1,10 +1,10 @@
 package com.vastika.training.java.cms.services;
 
 public interface BaseService {
-    public void displayMenu();
-    public void insertInfo();
-    public void viewInfo();
-    public void viewInfoById();
-    public void updateInfo();
-    public void deleteInfo();
+    void displayMenu();
+    void insertInfo();
+    void viewInfo();
+    void viewInfoById();
+    void updateInfo();
+    void deleteInfo();
 }
